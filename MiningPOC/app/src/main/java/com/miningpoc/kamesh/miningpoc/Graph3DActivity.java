84cc4c410a52a94eef264a0fa7d896bbad562e71
Kamesh <kasingh@groupon.com>
2016-08-06 10:38:52 +0530

    Added the data creation class.

--- MiningPOC/app/src/main/java/com/miningpoc/kamesh/miningpoc/Graph3DActivity.java
+++ MiningPOC/app/src/main/java/com/miningpoc/kamesh/miningpoc/Graph3DActivity.java
@@ -3,6 +3,13 @@ package com.miningpoc.kamesh.miningpoc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Random;
 
 public class Graph3DActivity extends AppCompatActivity {
 
@@ -15,5 +22,21 @@ public class Graph3DActivity extends AppCompatActivity {
         graph3dView = (WebView) findViewById(R.id.webView);
         graph3dView.getSettings().setJavaScriptEnabled(true);
         graph3dView.loadUrl("file:///android_asset/html/dot_colors.html");
+        final String jsonDataString = getDataAsJsonString();
+        graph3dView.setWebViewClient(new WebViewClient(){
+            public void onPageFinished(WebView view, String url){
+                graph3dView.loadUrl("javascript:drawWithData('" + jsonDataString + "')");
+            }
+        });
+    }
+
+    private String getDataAsJsonString() {
+        try {
+            JSONArray jsonData = DotData.getJSONArrayForData(100);
+            return jsonData.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
